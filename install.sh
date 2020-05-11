6696dd55868c39311873d84fbd0c7a308da37aad
notminerproduction <64098611+notminerproduction@users.noreply.github.com>
2020-05-11 19:53:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,17 +12,22 @@ sudo rm -rf /usr/lib/node_modules
 sudo apt remove --purge -y nodejs node
 sudo rm /etc/apt/sources.list.d/nodesource.list*
 sudo apt update
+sudo apt upgrade -y
+
+sudo apt install -y software-properties-common build-essential autoconf pkg-config make gcc g++ screen nano wget curl ntp fail2ban 
+
 sudo add-apt-repository -y ppa:chris-lea/redis-server
 sudo add-apt-repository -y ppa:bitcoin/bitcoin
 
 sudo apt update
-sudo apt upgrade -y
+sudo apt install -y libdb4.8-dev libdb4.8++-dev libssl-dev libboost-all-dev libminiupnpc-dev libtool autotools-dev redis-server
 
-sudo apt install -y screen make gcc g++ git nano wget curl ntp build-essential libtool autotools-dev autoconf pkg-config libssl-dev libboost-all-dev git libminiupnpc-dev redis-server software-properties-common fail2ban libdb4.8-dev libdb4.8++-dev
 sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
-
-sudo systemctl enable redis-server && sudo systemctl start redis-server && sudo systemctl enable ntp && sudo systemctl start ntp
+sudo systemctl enable redis-server
+sudo systemctl start redis-server
+sudo systemctl enable ntp
+sudo systemctl start ntp
 
 sudo rm -rf ~/.nvm
 sudo rm -rf ~/.npm
